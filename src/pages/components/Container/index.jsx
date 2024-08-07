b07d4135ffc1ba8074af97b19f415ba5e226a6b1
Ianlucca14 <124889802+Ianlucca14@users.noreply.github.com>
2024-08-07 11:39:12 -0300

    Update index.jsx

--- src/pages/components/Container/index.jsx
+++ src/pages/components/Container/index.jsx
@@ -10,9 +10,9 @@ function Container() {
          <a href="/teste3"> <input type="button" value="ESTRATEGIA-SANDBOX"  /></a> 
            <a href="/teste"> <input type="button" value="RPG DE AÇÃO" /></a>
             <div className={style.diva}>
-            <div className={style.button_slide}><a href="https://www.xbox.com/pt-BR"><img className={style.img}   src='../../../src/imagens/xboxxr.png'/></a></div>
-            <div className={style.button_nintendo}><a href="https://www.nintendo.com/pt-br/"><img className={style.img}   src='../../../src/imagens/nintendo.png'/></a></div>
-            <div className={style.button_play}><a href="https://www.playstation.com/pt-br/"><img className={style.imgplay}   src='../../../src/imagens/play.png'/></a></div>
+            <div className={style.button_slide}><a href="https://www.xbox.com/pt-BR"><img className={style.img}   src='../../../imagens/xboxxr.png'/></a></div>
+            <div className={style.button_nintendo}><a href="https://www.nintendo.com/pt-br/"><img className={style.img}   src='../../../imagens/nintendo.png'/></a></div>
+            <div className={style.button_play}><a href="https://www.playstation.com/pt-br/"><img className={style.imgplay}   src='../../../imagens/play.png'/></a></div>
            
             </div>
            </div>      
